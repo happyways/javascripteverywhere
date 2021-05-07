01ab4843899cb8c09d9ec947944e59291bcdd8c2
hojin_lee3 <hojin_lee3@tmax.co.kr>
2021-05-07 12:52:30 +0900

    포트 변경 가능하도록 수정 및 기본 포트 4000으로 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 // This is the main entry point of our application
 const express = require('express');
 const app = express();
+//port를 지정하지 않았으면 4000을 port로 사용
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World!!'));
 
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.listen(port, () =>
+    console.log('Listening on port 4000!')
+);
