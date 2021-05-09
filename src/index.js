1d669007eb521759d575e0219d07e8cae42276cc
happyways <dlhgwls8711@gmail.com>
2021-05-10 00:22:40 +0900

    tmp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-// index.js
 // This is the main entry point of our application
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
 //port를 지정하지 않았으면 4000을 port로 사용
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
 
-app.listen(port, () =>
-    console.log('Listening on port 4000!')
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  }
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    'GraphQL Server running at http://localhost:${port}${server.graphqlPath}'
+  )
 );
